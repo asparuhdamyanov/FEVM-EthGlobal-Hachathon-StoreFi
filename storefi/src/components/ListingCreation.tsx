b01ec6bbe2d11f470d72b3b056af099488f536f5
Martin Petrov <M.A.Petrov@student.tudelft.nl>
2022-11-19 20:01:37 +0100

    Add input fields

--- storefi/src/components/ListingCreation.tsx
+++ storefi/src/components/ListingCreation.tsx
@@ -1,23 +1,57 @@
 import React, {Component, JSXElementConstructor, ReactElement} from 'react';
-import { Input, TextFieldProps } from '@mui/material';
-import { DatePicker } from '@mui/x-date-pickers';
+import { Input, TextField, Button } from '@mui/material';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs, { Dayjs } from 'dayjs';
+
+
+
 import styles from './ListingCreation.module.css'
 
 function ListingCreation() {
+    const [value, setValue] = React.useState<Dayjs | null>(
+        dayjs('2014-08-18T21:11:54'),
+      );
+
+      const handleChange = (newValue: Dayjs | null) => {
+        setValue(newValue);
+      };
+
     return (
         <div className={styles.listingCreation}>
             <label>New Listing:</label>
-            <div>
+            <div className = {styles.inputItem}>
                 <label>Name of the listing: </label>
                 <Input></Input>
             </div>
             <div>
                 <label>Starting date of the listing: </label>
-                <input type='date' />
+                <div className = {styles.inputItem}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DateTimePicker
+                            label="Date & Time picker"
+                            value={value}
+                            onChange={handleChange}
+                            renderInput={(params) => <TextField {...params} />}
+                        >
+                        </DateTimePicker>
+                    </LocalizationProvider>
+                </div>
             </div>
             <div>
                 <label>Ending date of the listing: </label>
-                <input type = 'date'/>
+                <div className = {styles.inputItem}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DateTimePicker
+                            label="Date & Time picker"
+                            value={value}
+                            onChange={handleChange}
+                            renderInput={(params) => <TextField {...params} />}
+                        >
+                        </DateTimePicker>
+                    </LocalizationProvider>
+                </div>
             </div>
             <div>
                 <label>Starting price: </label>
@@ -27,6 +61,7 @@ function ListingCreation() {
                 <label>Immediate buy price: </label>
                 <Input></Input>
             </div>
+            <Button variant="outlined">Create</Button>
         </div>
     );
 }
