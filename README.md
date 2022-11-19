e17f42bbe160acc460e78811d3ab58182250a472
Martin Petrov <M.A.Petrov@student.tudelft.nl>
2022-11-19 18:33:09 +0100

    Create new react app

--- README.md
+++
@@ -1,8 +0,0 @@
-# FEVM-EthGlobal-Hachathon
-```shell
-# add the hash contract
-npm install
-npm run compile-contract-types
-npm run dev
-
-```
--- next.config.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-/**
- * @type {import('next').NextConfig}
- **/
-module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
