14f33f6b798467b8bb3fab827d7141ed82737cc1
petya0111 <pmarinova0111@gmail.com>
2022-11-19 15:00:06 +0200

    init frontend boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # FEVM-EthGlobal-Hachathon
+```shell
+# add the hash contract
+npm install
+npm run compile-contract-types
+npm run dev
+
+```
---
+++ constants/index.ts
@@ -0,0 +1,15 @@
+
+  export interface Networks {
+    [key: number]: string;
+  }
+  export const walletConnectSupportedNetworks: Networks = {
+    // Add your network rpc URL here
+    1: "https://ethereumnode.defiterm.io",
+    3: "https://ethereumnode.defiterm-dev.net"
+  };
+
+  // Network chain ids
+  export const supportedMetamaskNetworks = [1, 3, 4, 5, 42];
+
+  export const ALBT_TOKEN_ADDRESS = "0xc6869a93ef55e1d8ec8fdcda89c9d93616cf0a72";
+  export const US_ELECTION_ADDRESS = "0x259Aa3239b1e3498B8322447C860792e06B5D875";
